d262c055b568d5cb4b85c416a6c6d3511bd985da
Raynos <raynos2@gmail.com>
2012-08-06 17:49:19 -0700

    another bug. Same one.

--- example/simple/client.js
+++ example/simple/client.js
@@ -3,6 +3,11 @@ var boot = require('../../browser.js')
 
 var one = window.one = mdm.createStream("one")
 
+mdm.on("connect", function () {
+    // Race condition here :(
+    one.write("die!")
+})
+
 one.on("data", function (data) {
     console.log("message", data)
 })
